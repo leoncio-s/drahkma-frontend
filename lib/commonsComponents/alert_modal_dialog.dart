5e9ffbc87362905c90cd6d06a3e7f3068747f845
grrypsing <leonciosouza01@hotmail.com>
2025-11-20 22:07:47 -0300

    feat: criando tela para dialogos de alerta

---
+++ lib/commonsComponents/alert_modal_dialog.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+VoidCallback alertModalDialog<T>(
+  BuildContext context, T content, {double width=300.0, double height=200.0, String? title}){
+  Widget cont = content == "loading"? CircularProgressIndicator(color: Theme.of(context).primaryColorDark,) : Text(content as String,style: const TextStyle(color: Colors.black, fontSize: 18.0),);
+  
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (context)=>
+    AlertDialog(
+        scrollable: true,
+        title: Text(title ?? "", style: Theme.of(context).textTheme.titleMedium!.copyWith(color: Colors.red),),
+        actions: [
+          ElevatedButton(onPressed: (){Navigator.of(context).pop([true]);}, child: Text("Ok"))
+          ],
+        backgroundColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0)
+        ),
+        alignment: Alignment.center,
+        content:SizedBox(
+          width: width,
+          height: height,
+          child: Center(child:cont),
+        ),
+        ));
+        
+    return (){
+      Navigator.of(context).pop();
+    };
+}
