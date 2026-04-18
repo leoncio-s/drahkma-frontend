54af0102de9dde1a30780a2cec18a02e3d33cc88
grrypsing <leonciosouza01@hotmail.com>
2026-04-17 22:09:17 -0300

    feat: add new keys to storage

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -6,6 +6,12 @@ class Config{
   static String urlApi = 'http://localhost:8081/api/v1/';
   static String keyStorageAuthToken = "_auth_token";
   static String keyStorageEmail = '_email';
+  static String keyStorageUser = "_user_profile";
+  static String keyStorageCards = "_cards";
+  static String keyStorageCategories = "_categories";
+  static String keyStorageItems = "_items";
+  static String keyStorageBankAccounts = "_bank_accounts";
+  static String keyStorageDashboard = "_dashboard";
 
   static set setUrlApi(String value){
     urlApi = Uri.parse(value).toString();
