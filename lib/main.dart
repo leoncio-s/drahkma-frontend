1cec77ee011976242837d4c4c2d8af2d23192285
grrypsing <leonciosouza01@hotmail.com>
2026-04-20 21:50:44 -0300

    feat: add usePathUrlStrategy and fix error on navigation

--- lib/main.dart
+++ lib/main.dart
@@ -11,48 +11,60 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:drahkma/core/config.dart';
 import 'package:drahkma/di/injector.dart';
+import 'package:flutter_web_plugins/url_strategy.dart';
 
 late AppController appController;
 
 void exceptionHandler(Object e, StackTrace s) {
-  final BuildContext? context = AppRoutes.navigatorKey.currentContext;
+  final NavigatorState? navigator = AppRoutes.navigatorKey.currentState;
 
-  if (context == null) {
+  if (navigator == null) {
     log(e.toString(), stackTrace: s);
     return;
   }
 
-  try{
-    if (e is UnauthenticatedException) {
-    Navigator.of(context).pushReplacementNamed("login");
-    } else if (e is UpdatePasswordException) {
-      alertDialog<String>(context, e.message, title: "Erro ao atualizar senha");
-    } else {
+  Future.delayed(Duration.zero, () {
+    try {
+      if (e is UnauthenticatedException) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          Navigator.of(navigator.context).pushReplacementNamed(AppRoutes.login);
+        });
+      } else if (e is UpdatePasswordException) {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          alertDialog<String>(navigator.context, e.message,
+              title: "Erro ao atualizar senha");
+        });
+      } else {
+        log(e.toString(), stackTrace: s, level: 1, name: "Drahkma App");
+        debugPrintStack(stackTrace: s);
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          alertDialog<String>(navigator.context, e.toString(),
+              title: "Format Invalid Error");
+        });
+      }
+    } catch (dialogError, e) {
+      debugPrintStack(stackTrace: s, label: "Dialog Main Error Handler");
       log(e.toString(), stackTrace: s, level: 1, name: "Drahkma App");
-      debugPrintStack(stackTrace: s);
-      alertDialog<String>(context, e.toString(), title: "Format Invalid Error");
     }
-  } catch (dialogError, e)
-  {
-    debugPrintStack(stackTrace: s, label: "Dialog Main Error Handler");
-    log(e.toString(), stackTrace: s, level: 1, name: "Drahkma App");
-  }
+  });
 }
 
 void main() async {
-
+  usePathUrlStrategy();
   runZonedGuarded<void>(() async {
     WidgetsFlutterBinding.ensureInitialized();
+
     initializeDependencies();
     await getIt.allReady(timeout: Duration(seconds: 5));
+
     appController = getIt<AppController>();
 
     if (kDebugMode) {
       Config.setUrlApi = "http://localhost:8081/api/v1/";
     }
     await appController.checkNetwork();
-
     runApp(_buildApp());
+
   }, exceptionHandler);
 }
 
@@ -60,8 +72,7 @@ Widget _buildApp() {
   return ValueListenableBuilder<AppState>(
       valueListenable: appController,
       builder: (ctx, state, _) {
-        final String initialRoute =
-            state is NoNetworkState ? "error-page" : "login";
+        final String initialRoute = AppRoutes.login;
         return Drahkma(
           initialRoute: initialRoute,
         );
