5a7639fa1118dc1e811f90940f5921ace8e3605b
grrypsing <leonciosouza01@hotmail.com>
2026-05-08 23:49:41 -0300

    feat: update colors to hex default

--- lib/core/presentation/theme/app_colors.dart
+++ lib/core/presentation/theme/app_colors.dart
@@ -7,7 +7,7 @@ abstract class AppColors
   static const Color lightGold = Color(0xFFF9E79F); 
   static const Color expenseRed = Color(0xFFE57373); 
   static const Color incomeGreen = Color(0xFF81C784); 
-  static const Color lightBlue = Color.fromARGB(255, 142, 204, 255);
-  static const Color borderBlue = Color.fromARGB(255, 60, 96, 125);
+  static const Color lightBlue = Color(0xFF8ECCFF);
+  static const Color borderBlue = Color(0xFF3C607D);
   static const Color redError = Color(0xFFFF5080); 
 }
