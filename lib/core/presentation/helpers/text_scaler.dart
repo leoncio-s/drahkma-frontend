e38c27f4b6cb13b346780d938547397a6aa17b9a
grrypsing <leonciosouza01@hotmail.com>
2026-07-13 22:44:21 -0300

    feat: implementing responsive fontsize and textscaller

--- lib/core/presentation/helpers/text_scaler.dart
+++ lib/core/presentation/helpers/text_scaler.dart
@@ -1,11 +1,34 @@
-double principalCardScaller(double widthSize,{double maxscaller=2.0}){
+import 'package:flutter/material.dart';
+
+double responsiveFontSize(
+  BuildContext context, {
+  double factor = 0.045,
+  double min = 14,
+  double max = 22,
+}) {
+  final width = MediaQuery.of(context).size.width;
+  return (width * factor).clamp(min, max).toDouble();
+}
+
+TextScaler responsiveTextScaler(
+  BuildContext context, {
+  double minScaleFactor = 0.8,
+  double maxScaleFactor = 1.4,
+}) {
+  return MediaQuery.textScalerOf(context).clamp(
+    minScaleFactor: minScaleFactor,
+    maxScaleFactor: maxScaleFactor,
+  );
+}
+
+double principalCardScaller(double widthSize, {double maxscaller = 2.0}) {
   double scaller = 1.0;
-  if(widthSize < 200){
+  if (widthSize < 200) {
     scaller = 1.0;
-  }else{
+  } else {
     scaller = widthSize * 0.10;
   }
-  if(scaller > maxscaller){
+  if (scaller > maxscaller) {
     return maxscaller;
   }
   return scaller;
