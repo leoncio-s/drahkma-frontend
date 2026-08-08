83db125561f895589295164bed550a852440e9b8
grrypsing <leonciosouza01@hotmail.com>
2026-08-08 16:06:12 -0300

    refactore: remove unecessary catch and local storage

--- lib/features/bank_account/data/repositories/bank_accounts_repository_impl.dart
+++ lib/features/bank_account/data/repositories/bank_accounts_repository_impl.dart
@@ -1,3 +1,4 @@
+import 'package:drahkma/core/error/invalid_credentials_exception.dart';
 import 'package:drahkma/features/bank_account/data/sources/remote/bank_account_remote_datasource.dart';
 import 'package:drahkma/features/bank_account/data/sources/local/bank_account_local_datasource.dart';
 import 'package:drahkma/features/bank_account/domain/entities/bank.dart';
@@ -32,6 +33,8 @@ class BankAccountRepositoryImpl implements BankAccountRepository
         await _localDatasource.saveBankAccounts(accounts);
       }
       return accounts;
+    } on InvalidCredentialsException{
+      rethrow;
     } catch (e) {
       // Fallback to local datasource on network error
       return await _localDatasource.getBankAccounts();
@@ -46,22 +49,12 @@ class BankAccountRepositoryImpl implements BankAccountRepository
   @override
   Future<BankAccount?> save(BankAccount dto) async {
     BankAccount? savedAccount = await _remoteDatasource.save(dto);
-    if (savedAccount != null) {
-      await _localDatasource.saveBankAccounts([savedAccount]);
-    }
     return savedAccount;
   }
 
   @override
   Future<void> update(BankAccount dto) async {
-    try {
-      await _remoteDatasource.update(dto);
-      await _localDatasource.saveBankAccounts([dto]);
-    } catch (e) {
-      // On network error, at least save to local
-      await _localDatasource.saveBankAccounts([dto]);
-      rethrow;
-    }
+    await _remoteDatasource.update(dto);
   }
 
   @override
